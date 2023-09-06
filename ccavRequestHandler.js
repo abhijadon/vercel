fa6dc2610e198a345de94d5fb76352e18d891f94
abhijadon <134587271+abhijadon@users.noreply.github.com>
2023-09-06 11:34:48 +0530

    this

--- ccavRequestHandler.js
+++ ccavRequestHandler.js
@@ -5,20 +5,23 @@ var http = require("http"),
 
 exports.postReq = function (request, response) {
   var body = "",
-    workingKey = "9223AAA9021800C10C706B47E6B0D7C3", //Put in the 32-Bit Key provided by CCAvenue.
-    accessCode = "AVXS76JC64CK33SXKC", //Put in the Access Code provided by CCAvenue.
+    workingKey = "9223AAA9021800C10C706B47E6B0D7C3", //Put in the 32-Bit key shared by CCAvenues.
+    accessCode = "AVXS76JC64CK33SXKC", //Put in the access code shared by CCAvenues.
     encRequest = "",
     formbody = "";
 
   request.on("data", function (data) {
     body += data;
     encRequest = ccav.encrypt(body, workingKey);
+    POST = qs.parse(body);
     formbody =
-      '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
+      '<html><head><title>Sub-merchant checkout page</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script></head><body><center><!-- width required mininmum 482px --><iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=' +
+      POST.merchant_id +
+      "&encRequest=" +
       encRequest +
-      '"><input type="hidden" name="access_code" id="access_code" value="' +
+      "&access_code=" +
       accessCode +
-      '"><script language="javascript">document.redirect.submit();</script></form>';
+      '"></iframe></center><script type="text/javascript">$(document).ready(function(){$("iframe#paymentFrame").load(function() {window.addEventListener("message", function(e) {$("#paymentFrame").css("height",e.data["newHeight"]+"px"); }, false);}); });</script></body></html>';
   });
 
   request.on("end", function () {
